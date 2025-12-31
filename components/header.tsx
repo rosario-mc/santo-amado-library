1ee010d9800cbf3183afd3812132822331c96f1b
Personal <rosariomillercanales@gmail.com>
2025-12-30 20:58:43 -0800

    Updated UI to enhance user experience

--- components/header.tsx
+++ components/header.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 export default function Header() {
     return(
-        <header className="border-b border-gray-200">
+        <header className="border-b border-black">
             <nav className="flex items-center justify-between h-16">
                     <Link href="/" className="px-2 py-0 transition-transform hover:scale-105">
                         <Image
                             src="/header-logo.png"
                             alt="LibroLandia"
-                            width={180}
+                            width={200}
                             height={50}
                             priority
-                            className="animate-pulse hover:animate-bounce"
+                            className="hover:scale-105 hover:drop-shadow-lg transition-all duration-300"
                             />
                     </Link>
-                    <div className="flex gap-6 px-2 py-1">
+                    <div className="flex gap-6 px-2 py-1 text-black font-semibold">
                         <Link 
                           href="/library">
                           Books
