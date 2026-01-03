c3bde5bd946551be23a733cc7e0d7355c18933c9
Personal <rosariomillercanales@gmail.com>
2026-01-03 14:33:41 -0800

    Fixed error

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,7 @@
 export default function Footer() {
     return(
-<<<<<<< HEAD
-        <footer className="border-t border-black">
-            <div className="mx-auto max-w-7xl px-4 py-4">
-=======
-        <footer className="border-t border-black mt-auto">
+        <footer className="border-t border-black mt-8">
             <div className="mx-auto max-w-7xl px-4 py-8">
->>>>>>> main
                 <p className="text-center text-med text-black font-semibold">
                     © {new Date().getFullYear()} LibroLandia
                 </p>
